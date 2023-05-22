b5cb262bb342b89312f656275b30a0ad0ce90aed
reginald <reginaldosakue@gmail.com>
2023-05-22 15:55:26 -0400

    Completed Game of 5 rounds

--- external.js
+++ external.js
@@ -7,12 +7,15 @@ var Ran;
 Ran = Math.floor(Math.random() * 3);
 if (Ran == 0){
     console.log("Rock");
+    return "ROCK";
 }
 else if(Ran == 1){
     console.log("Paper");
+    return "PAPER";
 }
 else if(Ran == 2){
     console.log("Scissors");
+    return "SCISSORS";
 }
 }
 function RPS(playerSelection, computerSelection){
@@ -20,11 +23,68 @@ function RPS(playerSelection, computerSelection){
 Get the player Selection
 Use the getComputerChoice function for the computer selection
 allow  playerSelection parameter case-insensitive
-establish the winner of the round (Rock > Scissors) 
+establish the winner of the round (ex. Rock > Scissors) 
+Return winner
 */
-
-
+var pp=playerSelection.toUpperCase();
+if (pp=="ROCK" && computerSelection == "ROCK"){
+    console.log("Tie");
+    return 3;
+}
+else if (pp=="ROCK" && computerSelection == "PAPER"){
+    console.log("You Lose! Paper beats Rock");
+    return 2;
+}
+else if (pp=="ROCK" && computerSelection == "SCISSORS"){
+    console.log("You Win! Rock beats Scissors");
+    return 1;
+}
+else if (pp=="PAPER" && computerSelection == "PAPER"){
+    console.log("Tie");
+    return 3;
+}
+else if (pp=="PAPER" && computerSelection == "ROCK"){
+    console.log("You Win! Paper beats Rock");
+    return 1;
+}
+else if (pp=="PAPER" && computerSelection == "SCISSORS"){
+    console.log("You Lose! Scissors beats Paper");
+    return 2;
+}
+else if (pp=="SCISSORS" && computerSelection == "SCISSORS"){
+    console.log("Tie");
+    return 3;
+}
+else if (pp=="SCISSORS" && computerSelection == "PAPER"){
+    console.log("You Win! Scissors beats Paper");
+    return 1;
+}
+else if (pp=="SCISSORS" && computerSelection == "ROCK"){
+    console.log("You Lose! Rock beats Scissors");
+    return 2;
+}
 }
 
+function game(){
+var play=0;
+var com = 0;
+var tie =0;
+    for(let i=0; i<5; i++){
+       let playerSelection= prompt ("Please pick between Rock, Paper, Scissors" )
+       let computerSelection = getComputerChoice();
+            if (RPS(playerSelection, computerSelection)==1)
+                play++;
+            else if (RPS(playerSelection, computerSelection)==2)
+                com ++;
+            else if (RPS(playerSelection, computerSelection)==3)
+                tie ++;
+    }
+    if (play > com){
+        console.log("You win " + play + " to " + com);
+    }
+    else  if (play < com){
+        console.log("You lose " + play + " to " + com);
+    }
+}
 
 
